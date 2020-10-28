011aaeffd1640af3370f953443c74f17f9d9d27e
Dana <dclemmer2@mail.greenriver.edu>
2020-10-27 19:37:19 -0700

    form updated + form js

--- scripts/form.js
+++ scripts/form.js
@@ -1,17 +1,8 @@
-document.getElementById("form-main").onsubmit = validate;
+
 
 let formZip = document.getElementById("zip").value;
 if (formZip !== "zipNotListed" || formZip !== "none") {
 	showForms();
-}
-
-// make all error messages invisible again
-function clearErrors() {
-	// create node list of all elements with text-danger class
-	let errors = document.getElementsByClassName("text-danger");
-	for (let i = 0; i < errors.length; i++) {
-		errors[i].classList.add("d-none");
-	}
 }
 
 function showForms() {
@@ -21,7 +12,38 @@ function showForms() {
 	services.classList.remove("d-none");
 }
 
+
+//hide address section if no permanent residence
+document.getElementById("permRes").onclick = hideAddress;
+
+function hideAddress() {
+	let permResidence = document.getElementById("permRes");
+	let hide = document.getElementById("address-section");
+	if (permResidence.checked === true) {
+		hide.classList.add("d-none");
+	}
+	else {
+		hide.classList.remove("d-none");
+	}
+}
+
+
+//validate form fields
+document.getElementById("form-main").onsubmit = validate;
+
+
+// make all error messages invisible again
+function clearErrors() {
+	// create node list of all elements with text-danger class
+	let errors = document.getElementsByClassName("error");
+	for (let i = 0; i < errors.length; i++) {
+		errors[i].classList.add("d-none");
+	}
+}
+
+
 function validate() {
+
 	clearErrors();
 
 	let isValid = true;
@@ -42,14 +64,52 @@ function validate() {
 		isValid = false;
 	}
 
-	// error check agreement -- CURRENTLY BROKE - always displays error
-	// let agreement = document.getElementById("agreement").checked;
-	// if (!agreement.checked) {
-	// 	let errAgreement = document.getElementById("err-agreement");
-	// 	errAgreement.classList.remove("d-none");
-	// 	errAgreement.classList.add("d-block");
-	// 	isValid = false;
-	// }
+	//Validate zip code
+	let zip = document.getElementById("zip").value;
+	if(zip === "98030" || zip === "98031" || zip === "98032" || zip === "98042")
+		isValid = true;
+	else {
+		let errZip = document.getElementById("err-zip");
+		errZip.classList.remove("d-none");
+		isValid = false;
+	}
+
+	//Validate email:
+	let email = document.getElementById("email").value;
+	//if email is empty:
+	if (email !== "") {
+		if (!email.includes("@")) {
+			let invalidEmail = document.getElementById("invalid-email");
+			invalidEmail.classList.remove("d-none");
+			isValid = false;
+		}
+		if (!email.includes(".")) {
+			let invalidEmail = document.getElementById("invalid-email");
+			invalidEmail.classList.remove("d-none");
+			isValid = false;
+		}
+	}
+
+	//If both email and phone left blank:
+	let phone = document.getElementById("phone").value;
+	if(email === "" && phone === "") {
+		let errEmailPhone = document.getElementById("err-email-phone");
+		errEmailPhone.classList.remove("d-none");
+		isValid = false;
+	}
+
+
+	//Validate checkbox:
+	let agreement = document.getElementById("agreement");
+	//if checkbox not checked:
+	if (agreement.checked === false) {
+		let errAgreement= document.getElementById("err-agreement");
+		errAgreement.classList.remove("d-none");
+		isValid = false;
+	}
+
 
 	return isValid;
 }
+
+
