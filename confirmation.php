a7d154993237b38291e2064fd3f957cd557fe994
Dana <dclemmer2@mail.greenriver.edu>
2020-10-27 19:40:26 -0700

    confirmation.php after form filled out (?)

---
+++ confirmation.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/outreach_styles.css">
+    <title>Kent Outreach Program</title>
+    <!--Favicon-->
+    <link rel="icon" href="images/stjames_logo1.png">
+</head>
+<body>
+
+<div class="container" id="main">
+
+    <h1>Thank you for your request. We’ll be in touch soon!</h1>
+
+</div>
+
+</body>
+</html>
